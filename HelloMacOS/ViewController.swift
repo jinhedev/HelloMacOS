5db9239980dd8eb9df181eb8599a99eefb7a0ee5
rightmeow <rightmeow@rightmeows-MacBook-Pro.local>
2017-08-31 21:30:39 -0700

    init

---
+++ HelloMacOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HelloMacOS
+//
+//  Created by rightmeow on 8/31/17.
+//  Copyright © 2017 Duckensburg. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
